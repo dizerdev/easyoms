63d12a4e1b48fa94b58d6cc82e903efce6530946
Diego Santos <106088657+dizerdev@users.noreply.github.com>
2025-05-09 16:03:51 +0000

    Install `pg` module and define first version of `/api/v1/status`

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,6 +1,37 @@
-import { NextRequest, NextResponse } from 'next/server'
+import database from '@/infra/db/client'
+import { NextResponse } from 'next/server'
 
-export function GET(request: NextRequest) {
-  console.log(request)
-  return NextResponse.json({ chave: 'valor' }, { status: 200 })
+export async function GET() {
+  const updatedAt = new Date().toISOString()
+
+  const dataBaseVersionResult = await database.query('SHOW server_version;')
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version
+
+  const dataBaseMaxConnectionsResult = await database.query(
+    'SHOW max_connections;'
+  )
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections
+
+  const databaseName = process.env.POSTGRES_DB
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName],
+  })
+  const dataBaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count
+
+  return NextResponse.json(
+    {
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: dataBaseVersionValue,
+          max_connections: parseInt(dataBaseMaxConnectionsValue),
+          opened_connections: dataBaseOpenedConnectionsValue,
+        },
+      },
+    },
+    { status: 200 }
+  )
 }
