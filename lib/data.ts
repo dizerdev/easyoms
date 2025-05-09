4ee94660b651937d08994c28b2acea60fe86c6c7
Diego Santos <106088657+dizerdev@users.noreply.github.com>
2025-05-09 18:14:36 +0000

    feat: create shared website layout `header` and `footer`

---
+++ lib/data.ts
@@ -0,0 +1,38 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Ofertas Hoje",
+      href: "/search?tag=todays-deal",
+    },
+    {
+      name: "Lançamentos",
+      href: "/search?tag=new-arrival",
+    },
+    {
+      name: "Por Categoria",
+      href: "/search?tag=featured",
+    },
+    {
+      name: "Mais Vendidos",
+      href: "/search?tag=best-seller",
+    },
+    {
+      name: "Nossa História",
+      href: "/#browsing-history",
+    },
+    {
+      name: "Serviços",
+      href: "/page/customer-service",
+    },
+    {
+      name: "Sobre",
+      href: "/page/about-us",
+    },
+    {
+      name: "Contato",
+      href: "/page/help",
+    },
+  ],
+};
+
+export default data;
