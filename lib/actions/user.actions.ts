56103c26f6d4d150f6261cfa3e2055ac7bcbd962
Diego Santos <zerbiniprogramador@gmail.com>
2025-05-21 09:02:11 -0300

    feat: add sign in with `google`

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -11,9 +11,14 @@ import { redirect } from "next/navigation";
 export async function signInWithCredentials(user: IUserSignIn) {
   return await signIn("credentials", { ...user, redirect: false });
 }
+
 export const SignOut = async () => {
   const redirectTo = await signOut({ redirect: false });
   redirect(redirectTo.redirect);
+};
+
+export const SignInWithGoogle = async () => {
+  await signIn("google");
 };
 
 // CREATE
