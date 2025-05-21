340f6cda8ad5058a525fa63d1f59bbc70f97e700
Diego Santos <zerbiniprogramador@gmail.com>
2025-05-21 18:23:26 -0300

    feat: connect to `paypal` and `resend`

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,13 +1,25 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Easyoms";
+
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || "onboarding@resend.dev";
+
+export const SENDER_NAME = process.env.SENDER_NAME || APP_NAME;
+
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || "Seu jeito fácil de comprar.";
+
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "An Ecommerce website using Next.js, Postgres and AWS S3";
+
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
+
 export const FREE_SHIPPING_MIN_PRICE = Number(
   process.env.FREE_SHIPPING_MIN_PRICE || 35,
 );
+
 export const APP_COPYRIGHT =
   process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
   `Copyright © 2025 ${APP_NAME}. All rights reserved.`;
