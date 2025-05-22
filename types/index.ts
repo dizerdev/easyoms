c173bc1311c56183e3a874a22e30ec600b03340f
Diego Santos <zerbiniprogramador@gmail.com>
2025-05-22 08:38:00 -0300

    feat: create `reviwes` component

--- types/index.ts
+++ types/index.ts
@@ -7,13 +7,28 @@ import {
   UserInputSchema,
   UserSignInSchema,
   UserSignUpSchema,
+  ReviewInputSchema,
 } from "@/lib/validator";
 import { z } from "zod";
+
+export type IReviewInput = z.infer<typeof ReviewInputSchema>;
+export type IReviewDetails = IReviewInput & {
+  _id: string;
+  createdAt: string;
+  user: {
+    name: string;
+  };
+};
 
 export type IProductInput = z.infer<typeof ProductInputSchema>;
 export type Data = {
   users: IUserInput[];
   products: IProductInput[];
+  reviews: {
+    title: string;
+    rating: number;
+    comment: string;
+  }[];
   headerMenus: {
     name: string;
     href: string;
