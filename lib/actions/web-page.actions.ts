5d35bfc46a7da6d197d80e28ec3457807fe8f2df
Diego Santos <zerbiniprogramador@gmail.com>
2025-05-24 16:52:46 -0300

    feat: add create and update `/admin/web-page`

--- lib/actions/web-page.actions.ts
+++ lib/actions/web-page.actions.ts
@@ -5,8 +5,39 @@ import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "@/lib/db";
 import WebPage, { IWebPage } from "@/lib/db/models/web-page.model";
 import { formatError } from "@/lib/utils";
+import { WebPageInputSchema, WebPageUpdateSchema } from "../validator";
+import { z } from "zod";
 
-// DELETE
+export async function createWebPage(data: z.infer<typeof WebPageInputSchema>) {
+  try {
+    const webPage = WebPageInputSchema.parse(data);
+    await connectToDatabase();
+    await WebPage.create(webPage);
+    revalidatePath("/admin/web-pages");
+    return {
+      success: true,
+      message: "WebPage created successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+export async function updateWebPage(data: z.infer<typeof WebPageUpdateSchema>) {
+  try {
+    const webPage = WebPageUpdateSchema.parse(data);
+    await connectToDatabase();
+    await WebPage.findByIdAndUpdate(webPage._id, webPage);
+    revalidatePath("/admin/web-pages");
+    return {
+      success: true,
+      message: "WebPage updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
 export async function deleteWebPage(id: string) {
   try {
     await connectToDatabase();
@@ -22,7 +53,6 @@ export async function deleteWebPage(id: string) {
   }
 }
 
-// GET ALL
 export async function getAllWebPages() {
   await connectToDatabase();
   const webPages = await WebPage.find();
@@ -34,7 +64,6 @@ export async function getWebPageById(webPageId: string) {
   return JSON.parse(JSON.stringify(webPage)) as IWebPage;
 }
 
-// GET ONE PAGE BY SLUG
 export async function getWebPageBySlug(slug: string) {
   await connectToDatabase();
   const webPage = await WebPage.findOne({ slug, isPublished: true });
